b8a1664b9bfd23b00f56ec5b83071765adfd9fbb
Dave Head-Rapson <dave@dhrapson.com>
2016-11-30 20:31:00 +0000

    convert to gexec / gbytes for testing execution and output

--- main_test.go
+++ main_test.go
@@ -5,44 +5,76 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
 )
-
-func runCli(args ...string) (string, error) {
-	cmd := exec.Command("sched-load", args...)
-	outBytes, err := cmd.CombinedOutput()
-	return string(outBytes), err
-}
 
 var _ = Describe("SchedLoad", func() {
 
-	Context("When run with status argument", func() {
-		It("exits nicely", func() {
-			stringArgs := []string{"status"}
-			output, err := runCli(stringArgs...)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(output).To(ContainSubstring("status"))
-		})
+	var (
+		cliPath string
+		session *Session
+		err     error
+		args    []string
+	)
 
+	BeforeSuite(func() {
+		cliPath, err = Build("github.com/dhrapson/sched-load")
+		Ω(err).ShouldNot(HaveOccurred(), "Error building source")
 	})
 
-	Context("When run with help argument", func() {
-		It("exits nicely", func() {
-			stringArgs := []string{"help"}
-			output, err := runCli(stringArgs...)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(output).To(ContainSubstring("help"))
-		})
-
+	AfterSuite(func() {
+		CleanupBuildArtifacts()
 	})
 
-	Context("When run with an invalid argument", func() {
-		It("exits with non-zero error code", func() {
-			stringArgs := []string{"blah"}
-			output, err := runCli(stringArgs...)
-			Expect(err).To(HaveOccurred())
-			Expect(output).To(ContainSubstring("Invalid"))
+	Describe("invoking correctly", func() {
+
+		JustBeforeEach(func() {
+			command := exec.Command(cliPath, args...)
+			session, err = Start(command, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred(), "Error running CLI: "+cliPath)
+			Eventually(session).Should(Exit(0), cliPath+" exited with non-zero error code")
 		})
 
+		Context("When run with status argument", func() {
+			BeforeEach(func() {
+				args = []string{"status"}
+			})
+
+			It("exits nicely", func() {
+				Ω(session.Out).Should(Say("status"))
+			})
+		})
+
+		Context("When run with help argument", func() {
+			BeforeEach(func() {
+				args = []string{"help"}
+			})
+
+			It("exits nicely", func() {
+				Ω(session.Out).Should(Say("help"))
+			})
+		})
 	})
 
+	Describe("invoking incorrectly", func() {
+
+		JustBeforeEach(func() {
+			command := exec.Command(cliPath, args...)
+			session, err = Start(command, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred(), "Error running CLI: "+cliPath)
+			Eventually(session).Should(Exit(1), cliPath+" exited with unexpected error code")
+		})
+
+		Context("When run with an invalid argument", func() {
+			BeforeEach(func() {
+				args = []string{"foo"}
+			})
+
+			It("exits with non-zero error code", func() {
+				Ω(session.Err).Should(Say("Invalid"))
+			})
+
+		})
+	})
 })
