0182a2f2257a45415e64aadc6d0e5f23767ecb6e
Dave Head-Rapson <dave@dhrapson.com>
2016-11-30 19:07:57 +0000

    remove unnecessary descriptions

--- main_test.go
+++ main_test.go
@@ -15,36 +15,34 @@ func runCli(args ...string) (string, error) {
 
 var _ = Describe("SchedLoad", func() {
 
-	Describe("running the CLI", func() {
-		Context("When run with status argument", func() {
-			It("exits nicely", func() {
-				stringArgs := []string{"status"}
-				output, err := runCli(stringArgs...)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(output).To(ContainSubstring("status"))
-			})
-
+	Context("When run with status argument", func() {
+		It("exits nicely", func() {
+			stringArgs := []string{"status"}
+			output, err := runCli(stringArgs...)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(ContainSubstring("status"))
 		})
 
-		Context("When run with help argument", func() {
-			It("exits nicely", func() {
-				stringArgs := []string{"help"}
-				output, err := runCli(stringArgs...)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(output).To(ContainSubstring("help"))
-			})
+	})
 
+	Context("When run with help argument", func() {
+		It("exits nicely", func() {
+			stringArgs := []string{"help"}
+			output, err := runCli(stringArgs...)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(ContainSubstring("help"))
 		})
 
-		Context("When run with an invalid argument", func() {
-			It("exits with non-zero error code", func() {
-				stringArgs := []string{"blah"}
-				output, err := runCli(stringArgs...)
-				Expect(err).To(HaveOccurred())
-				Expect(output).To(ContainSubstring("Invalid"))
-			})
+	})
 
+	Context("When run with an invalid argument", func() {
+		It("exits with non-zero error code", func() {
+			stringArgs := []string{"blah"}
+			output, err := runCli(stringArgs...)
+			Expect(err).To(HaveOccurred())
+			Expect(output).To(ContainSubstring("Invalid"))
 		})
+
 	})
 
 })
