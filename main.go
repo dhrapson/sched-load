39e1f011eae64b4780efb6af583cd8b7631f391f
Dave Head-Rapson <dave@dhrapson.com>
2016-12-22 14:36:26 +0000

    switch implementation to use CLI library commands feature

--- main.go
+++ main.go
@@ -4,43 +4,42 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 
 	"github.com/urfave/cli"
 )
 
 func isValidActionCommand(actionCommand string) bool {
-	actions := []string{"status", "help"}
-	sort.Strings(actions)
-	i := sort.SearchStrings(actions, actionCommand)
-	if i < len(actions) && actions[i] == actionCommand {
-		return true
+	actions := []string{"status", "help", "create"}
+	for _, action := range actions {
+		if action == actionCommand {
+			return true
+		}
 	}
 	return false
 }
 
-func Run(args []string) {
-	fmt.Println("Running cli with", args)
+func main() {
 	app := cli.NewApp()
+
 	app.Name = "sched-load"
 	app.Usage = "uploads files to public IaaS & publishes a schedule for regular file uploads"
 
-	app.Action = func(c *cli.Context) error {
-		actionName := "help"
-		if c.NArg() > 0 {
-			actionName = c.Args().Get(0)
-		}
-		if isValidActionCommand(actionName) {
-			fmt.Println("Running ", actionName)
-		} else {
-			log.Fatal("Invalid action command: ", actionName)
-		}
-		return nil
+	app.Commands = []cli.Command{
+		{
+			Name:    "status",
+			Aliases: []string{"s"},
+			Usage:   "show status of connection and schedule",
+			Action: func(c *cli.Context) error {
+				fmt.Println("status")
+				return nil
+			},
+		},
 	}
 
-	app.Run(args)
-}
-
-func main() {
-	Run(os.Args)
+	app.CommandNotFound = func(c *cli.Context, command string) {
+		log.Printf("Invalid command '%s'\n\n", command)
+		cli.ShowAppHelp(c)
+		os.Exit(1)
+	}
+	app.Run(os.Args)
 }
