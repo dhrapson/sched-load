cfebe26c47b9b05505061095efe100d6f22eefc1
DHR <dave@dhrapson.com>
2017-01-04 16:01:21 +0000

    add iaas client

---
+++ iaas/iaas.go
@@ -0,0 +1,63 @@
+package iaas
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+	"strings"
+)
+
+type IaaSClient struct {
+	Region       string
+	session      *session.Session
+	IntegratorId string
+	ClientId     string
+}
+
+func (client IaaSClient) ListFiles() (names []string, err error) {
+	names = []string{}
+
+	session, err := client.connect()
+	if err != nil {
+		log.Println("Failed to connect", err)
+		return
+	}
+
+	svc := s3.New(session)
+
+	params := &s3.ListObjectsInput{
+		Bucket: aws.String("/" + client.IntegratorId),
+		Prefix: aws.String(client.ClientId),
+	}
+	resp, err := svc.ListObjects(params)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	for _, key := range resp.Contents {
+		objectPath := *key.Key
+		objectName := strings.Join(strings.Split(objectPath, "/")[1:], "/")
+		names = append(names, objectName)
+	}
+	return
+}
+
+func (client IaaSClient) connect() (sess *session.Session, err error) {
+	sess, err = session.NewSession(&aws.Config{
+		Region: aws.String(client.Region),
+	})
+
+	if err != nil {
+		log.Println("Failed to connect", err)
+	}
+
+	_, err = sess.Config.Credentials.Get()
+	if err != nil {
+		log.Println("Credentials not set", err)
+	}
+
+	return
+}
---
+++ iaas/iaas_test.go
@@ -0,0 +1,67 @@
+package iaas_test
+
+import (
+	. "github.com/dhrapson/sched-load/iaas"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"os"
+)
+
+var (
+	client          IaaSClient
+	accessKeyId     string
+	secretAccessKey string
+	region          string
+)
+
+var _ = Describe("The IaaS Client", func() {
+
+	BeforeSuite(func() {
+		accessKeyId = os.Getenv("TEST_AWS_ACCESS_KEY_ID")
+		secretAccessKey = os.Getenv("TEST_AWS_SECRET_ACCESS_KEY")
+		Ω(accessKeyId).ShouldNot(BeEmpty(), "You must set TEST_AWS_ACCESS_KEY_ID environment variable")
+		Ω(secretAccessKey).ShouldNot(BeEmpty(), "You must set TEST_AWS_SECRET_ACCESS_KEY environment variable")
+	})
+
+	JustBeforeEach(func() {
+		client = IaaSClient{IntegratorId: "test-integrator", ClientId: "test-client", Region: region}
+	})
+
+	Describe("Connecting", func() {
+
+		var (
+			result []string
+			err    error
+		)
+
+		JustBeforeEach(func() {
+			result, err = client.ListFiles()
+		})
+
+		Context("with valid connection details", func() {
+			BeforeEach(func() {
+				os.Setenv("AWS_ACCESS_KEY_ID", accessKeyId)
+				os.Setenv("AWS_SECRET_ACCESS_KEY", secretAccessKey)
+				region = "eu-west-1"
+			})
+
+			AfterEach(func() {
+				os.Unsetenv("AWS_ACCESS_KEY_ID")
+				os.Unsetenv("AWS_SECRET_ACCESS_KEY")
+			})
+
+			It("connects correctly & can see the contents", func() {
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(result).Should(ContainElement("test-folder/"))
+			})
+		})
+
+		Context("with invalid connection details", func() {
+
+			It("throws an error", func() {
+				Ω(err).Should(HaveOccurred())
+			})
+		})
+	})
+})
